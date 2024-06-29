e40000064d0d368f379cbd19ef535559bbecc536
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-06-29 22:02:11 +0900

    [뱀서/애니메이션/야준서] 플레이어 애니메이션 추가

--- VampireSurvivor/Assets/Scripts/Player.cs
+++ VampireSurvivor/Assets/Scripts/Player.cs
@@ -6,7 +6,11 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D _rigidBody = null;
+    [SerializeField] SpriteRenderer _spriteRender = null;
+    [SerializeField] Animator _animator = null;
     [SerializeField] float _speed = 3.0f;
+
+    private const string ANIMATOR_PARAMETERS_SPEED = "Speed";
 
     Vector2 _inputVec;
 
@@ -18,12 +22,31 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 movePos = _inputVec.normalized * _speed * Time.fixedDeltaTime;
-        _rigidBody.MovePosition(_rigidBody.position + movePos);
+        if (_rigidBody != null)
+        {
+            Vector2 movePos = _inputVec.normalized * _speed * Time.fixedDeltaTime;
+            _rigidBody.MovePosition(_rigidBody.position + movePos);
+        }
     }
 
     private void OnMove(InputValue value)
     {
         _inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        if(_animator != null)
+        {
+            _animator.SetFloat(ANIMATOR_PARAMETERS_SPEED, _inputVec.magnitude);
+        }
+
+        if (_spriteRender != null)
+        {
+            if (_inputVec.x != 0)
+            {
+                _spriteRender.flipX = _inputVec.x < 0;
+            }
+        }
     }
 }
